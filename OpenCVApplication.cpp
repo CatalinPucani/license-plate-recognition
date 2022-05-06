704c9c5b17d3c2a7f32dab20b85345ba5bf28dff
CatalinPucani <catalin@DESKTOP-T4V6DFS.localdomain>
2022-05-06 17:44:33 +0300

    morphological operations

--- OpenCVApplication.cpp
+++ OpenCVApplication.cpp
@@ -419,9 +419,444 @@ void showHistogram(const std::string& name, int* hist, const int  hist_cols, con
 	imshow(name, imgHist);
 }
 
+Mat convolutie(Mat src, Mat kernel, float m)
+{
+	float S = 0;
+	float k = 0;
+
+	Mat dst = Mat(src.size(), CV_8UC1);
+
+	k = kernel.cols / 2;
+
+	if (k < 2) k = kernel.rows / 2;
+
+
+	dst.convertTo(dst, CV_32FC1);
+
+	for (int i = k; i < src.rows - k; i++) {
+		for (int j = k; j < src.cols - k; j++) {
+			S = 0.0;
+			for (int ki = 0; ki < kernel.rows; ki++)
+				for (int kj = 0; kj < kernel.cols; kj++) {
+					S += (1 / m) * src.at<byte>(i + ki - k, j + kj - k) * kernel.at<float>(ki, kj);
+				}
+			dst.at<float>(i, j) = S;
+		}
+	}
+
+	dst.convertTo(dst, CV_8UC1);
+
+	return dst;
+}
+
+Mat Gaussian2D(Mat src, int n) {
+	double t = (double)getTickCount();
+
+	Mat dst;
+	char fname[MAX_PATH];
+	float data[100];
+
+
+	float sigma = (float)n / 6;
+	printf("%f\n", sigma);
+
+	Mat kernel = Mat(n, n, CV_32F);
+
+	float sum = 0;
+
+		for (int ki = 0; ki < n; ki++) {
+
+			for (int kj = 0; kj < n; kj++) {
+
+				float intermed = -((ki - n / 2) * (ki - n / 2) + (kj - n / 2) * (kj - n / 2)) / (2 * sigma * sigma);
+
+				kernel.at<float>(ki, kj) = (1 / (2 * PI * sigma * sigma)) * exp(intermed);
+
+				sum += kernel.at<float>(ki, kj);
+			}
+		}
+
+		dst = convolutie(src, kernel, sum);
+
+		t = ((double)getTickCount() - t) / getTickFrequency();
+
+		printf("Time = %.3f [s]\n", t);
+
+		imshow("src", src);
+		imshow("dst", dst);
+
+	return dst;
+}
+
+bool isInside(Mat img, int i, int j) {
+	return (i >= 0 && i < img.rows&& j >= 0 && j < img.cols);
+}
+
+int* makeHistogram(Mat src)
+{
+	int* histogram = (int*)calloc(256, sizeof(int));
+
+	for (int i = 0; i < src.rows; i++)
+		for (int j = 0; j < src.cols; j++)
+			histogram[src.at<uchar>(i, j)]++;
+
+	return histogram;
+}
+
+Mat auto_threshold(Mat img) {
+	float mg1 = 0, mg2 = 0;
+	int N1 = 0, N2 = 0;
+	float error=0.1;
+	Mat dst;
+	dst = Mat(img.rows, img.cols, CV_8UC1);
+	int* histogram = makeHistogram(img);
+	int min = histogram[0];
+		int max = histogram[0];
+		bool found = false;
+		for (int i = 0; i < 256; ++i) {
+			if (histogram[i] != 0) {
+				if (found == false) {
+					min = i;
+					found = true;
+				}
+				else {
+					max = i;
+				}
+
+			}
+		}
+		float Tk = (min + max) / 2;
+		float Tk_1 = 0;
+		do {
+			for (int g = min; g < Tk; g++) {
+				mg1 += g * histogram[g];
+				N1 += histogram[g];
+			}
+
+			for (int g = Tk + 1; g < max; g++) {
+				mg2 += g * histogram[g];
+				N2 += histogram[g];
+			}
+
+			mg1 = mg1 / N1;
+			mg2 = mg2 / N2;
+
+			Tk_1 = Tk;
+			Tk = (mg1 + mg2) / 2;
+		} while (abs(Tk - Tk_1) < error);
+		for (int i = 0; i < img.rows; i++)
+			for (int j = 0; j < img.cols; j++) {
+				if (img.at<uchar>(i, j) < Tk)
+					dst.at<uchar>(i, j) = 0;
+				else
+					dst.at<uchar>(i, j) = 255;
+			}
+		//imshow("Source", img);
+		//showHistogram("Histogram", histogram, 256, 200);
+		//showHistogram("Histogram_after", makeHistogram(dst), 256, 200);
+		//printf("pragul este: %.2f", Tk);
+		//imshow("Destination", dst);
+		//waitKey(0);
+	
+	return dst;
+}
+
+void centering_transform(Mat img) {
+	for (int i = 0; i < img.rows; i++) {
+		for (int j = 0; j < img.cols; j++) {
+			img.at<float>(i, j) = ((i + j) & 1) ? -img.at<float>(i, j) : img.at<float>(i, j);
+		}
+	}
+}
+
+Mat sablon_filtru_gaussian_high_pass(Mat src) {
+
+		Mat srcf(src.rows, src.cols, CV_32FC1); //stocam imaginea sursa ca float
+		src.convertTo(srcf, CV_32FC1);
+
+		centering_transform(srcf); //centram imaginea, pentru a o putea procesa in domeniul frecvential
+
+		Mat fourier; //cream o matrice de numere complexe in care sa stocam transformata fourier directa
+		dft(srcf, fourier, DFT_COMPLEX_OUTPUT); //aplicam transformata fourier directa pe imaginea sursa si rezultatul va fi stocat in matricea "fourier" de numere complexe
+
+		//dorim sa desfacem numerele complexe din matricea "fourier" in doua matrici, una cu partile reale si una cu partile imaginare ale numerelor 
+		Mat channels[] = { Mat::zeros(src.size(),CV_32F),Mat::zeros(src.size(),CV_32F) };
+		split(fourier, channels);//separam partea reala de partea imaginara
+		//in matricea channels[0] avem matricea care contine partile reale ale numerelor din "fourier"
+		//in matricea channels[1] avem partea imaginara a numerelor
+
+				//calculul magnitudinii
+
+
+		//aici aplicam filtre (R=20 sau A=20)
+		//parcurgem imaginea (0->H, 0->W) si aplicam modificarile pe channels[0] si channels[1]
+		//pentru gauss putem folosi functia exp(exponent)
+
+			//exemplu pentru filtru gaussian low pass
+		int H = src.rows;
+		int W = src.cols;
+		int A = 20;
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < W; j++) {
+				channels[0].at<float>(i, j) *= (1 - exp(-((H / 2 - i) * (H / 2 - i) + (W / 2 - j) * (W / 2 - j)) / (A * A)));
+				channels[1].at<float>(i, j) *= (1 - exp(-((H / 2 - i) * (H / 2 - i) + (W / 2 - j) * (W / 2 - j)) / (A * A)));
+			}
+		}
+
+		Mat dst, dstf;
+		merge(channels, 2, fourier); //reunim cele doua canale (real si imaginar), pentru a pregati matricea de revenirea in domeniul spatial
+		dft(fourier, dstf, DFT_INVERSE | DFT_REAL_OUTPUT | DFT_SCALE); //aplicam transformata fourier inversa pentru a reveni in domeniul spatial
+		//in matricea dstf avem rezultatul transformatei fourier inverse
+
+		centering_transform(dstf); //recentram imaginea, pentru a o vizualiza usor in domeniul spatial
+		normalize(dstf, dst, 0, 255, NORM_MINMAX, CV_8UC1); //normalizam matricea, ca sa nu avem valori in afara intervalului 0-255; punem rezultatul in dst
+
+		//imshow("initial", src);
+		//imshow("final", dst);
+		//waitKey();
+
+		return dst;
+
+}
+
+Mat sablon_filtru_ideal_high_pass(Mat src) {
+	
+		Mat srcf(src.rows, src.cols, CV_32FC1); //stocam imaginea sursa ca float
+		src.convertTo(srcf, CV_32FC1);
+
+		centering_transform(srcf); //centram imaginea, pentru a o putea procesa in domeniul frecvential
+
+		Mat fourier; //cream o matrice de numere complexe in care sa stocam transformata fourier directa
+		dft(srcf, fourier, DFT_COMPLEX_OUTPUT); //aplicam transformata fourier directa pe imaginea sursa si rezultatul va fi stocat in matricea "fourier" de numere complexe
+
+		//dorim sa desfacem numerele complexe din matricea "fourier" in doua matrici, una cu partile reale si una cu partile imaginare ale numerelor 
+		Mat channels[] = { Mat::zeros(src.size(),CV_32F),Mat::zeros(src.size(),CV_32F) };
+		split(fourier, channels);//separam partea reala de partea imaginara
+		//in matricea channels[0] avem matricea care contine partile reale ale numerelor din "fourier"
+		//in matricea channels[1] avem partea imaginara a numerelor
+
+
+		//aici aplicam filtre (R=20 sau A=20)
+		//parcurgem imaginea (0->H, 0->W) si aplicam modificarile pe channels[0] si channels[1]
+		//pentru gauss putem folosi functia exp(exponent)
+
+			//exemplu pentru filtru gaussian low pass
+		int H = src.rows;
+		int W = src.cols;
+		int R = 20;
+		for (int i = 0; i < H; i++) {
+			for (int j = 0; j < W; j++) {
+				if (((H / 2 - i) * (H / 2 - i) + (W / 2 - j) * (W / 2 - j)) <= R * R) {
+					channels[0].at<float>(i, j) = 0;
+					channels[1].at<float>(i, j) = 0;
+				}
+			}
+		}
+
+		Mat dst, dstf;
+		merge(channels, 2, fourier); //reunim cele doua canale (real si imaginar), pentru a pregati matricea de revenirea in domeniul spatial
+		dft(fourier, dstf, DFT_INVERSE | DFT_REAL_OUTPUT | DFT_SCALE); //aplicam transformata fourier inversa pentru a reveni in domeniul spatial
+		//in matricea dstf avem rezultatul transformatei fourier inverse
+
+		centering_transform(dstf); //recentram imaginea, pentru a o vizualiza usor in domeniul spatial
+		normalize(dstf, dst, 0, 255, NORM_MINMAX, CV_8UC1); //normalizam matricea, ca sa nu avem valori in afara intervalului 0-255; punem rezultatul in dst
+
+	
+
+		return dst;
+	
+}
+
+Mat getS()
+{
+	Mat S(3, 3, CV_8UC1);
+	S.at<uchar>(0, 0) = 0;
+	S.at<uchar>(0, 1) = 1;
+	S.at<uchar>(0, 2) = 0;
+	S.at<uchar>(1, 0) = 1;
+	S.at<uchar>(1, 1) = 1;
+	S.at<uchar>(1, 2) = 1;
+	S.at<uchar>(2, 0) = 0;
+	S.at<uchar>(2, 1) = 1;
+	S.at<uchar>(2, 2) = 0;
+
+	return S;
+}
+
+Mat dilatare(Mat src, int N) {
+
+	Mat dst = Mat(src.size(), CV_8UC1);
+
+	char fname[MAX_PATH];
+
+
+	dst = src.clone();
+	int w = 3;
+
+	Mat S = getS();
+
+	for (int k = 0; k < N; k++) {
+		for (int i = 0; i < src.rows; i++) {
+			for (int j = 0; j < src.cols; j++) {
+				if (src.at<uchar>(i, j) == 0) {
+					for (int x = 0; x < S.rows; x++) {
+						for (int y = 0; y < S.cols; y++) {
+							if (S.at<uchar>(x, y) == 1) {
+								int auxx = i + x - (S.rows / 2);
+								int auxy = j + y - (S.cols / 2);
+								if (isInside(src, auxx, auxy)) {
+									dst.at<uchar>(auxx, auxy) = 0;
+								}
+							}
+
+						}
+					}
+				}
+			}
+		}
+		src = dst.clone();
+	}
+
+	//imshow("dilatare", dst);
+
+	return dst;
+}
+
+
+
+Mat eroziune(Mat src, int N) {
+	Mat dst = Mat(src.size(), CV_8UC1);
+	char fname[MAX_PATH];
+
+
+	dst = src.clone();
+	int w = 3;
+
+	Mat S = getS();
+
+	bool whiteFlag = false;
+
+	for (int n = 0; n < N; n++) {
+		for (int i = 0; i < src.rows; i++) {
+			for (int j = 0; j < src.cols; j++) {
+				if (src.at<uchar>(i, j) == 0) {
+					for (int x = 0; x < S.rows; x++) {
+						for (int y = 0; y < S.cols; y++) {
+							int auxx = i + x - (S.rows / 2);
+							int auxy = j + y - (S.cols / 2);
+							if (isInside(src, auxx, auxy) && S.at<uchar>(x, y) == 1 && src.at<uchar>(auxx, auxy) == 255) {
+								dst.at<uchar>(i, j) = 255;
+							}
+
+						}
+					}
+
+				}
+			}
+		}
+		src = dst.clone();
+	}
+
+	//imshow("eroziune", dst);
+
+	return dst;
+}
+
+Mat deschidere(Mat src, int N) {
+
+	//Mat dst = src.clone();
+	//dst = convertToBlackAndWhiteSrc(src);
+
+	Mat dst;
+	for (int k = 0; k < N; k++) {
+		dst = eroziune(src, 1);
+		dst = dilatare(dst, 1);
+		
+	}
+
+	//imshow("inchidere", dst);
+
+	return dst;
+}
+
+
+Mat maximumFilter(Mat img)
+{
+		Mat dst = Mat(img.rows, img.cols, CV_8UC1);
+
+		dst = img.clone();
+
+		int matrix[3][3];
+		matrix[0][0] = -1;
+		matrix[0][1] = -1;
+		matrix[0][2] = -1;
+		matrix[1][0] = -1;
+		matrix[1][1] = 8;
+		matrix[1][2] = -1;
+		matrix[2][0] = -1;
+		matrix[2][1] = -1;
+		matrix[2][2] = -1;
+
+		int array[9] = { 0 };
+
+
+		for (int i = 0; i < img.rows; i++) {
+			for (int j = 0; j < img.cols; j++) {
+				int sum = 0;
+				int k = 0;
+				for (int ii = 0; ii < 3; ii++) {
+					for (int jj = 0; jj < 3; jj++) {
+						if (isInside(img, i + ii, j + jj)) {
+							array[k++] = img.at<uchar>(i + ii, j + jj);
+						}
+					}
+					//printf("%d\n", sum);
+				}
+				//sum /= 9;
+				if (isInside(img, i + 1, j + 1)) {
+					std::sort(array, array + 9);
+					dst.at<uchar>(i + 1, j + 1) = array[8];
+
+				}
+			}
+		}
+
+		return dst;
+}
+
+void getLicensePlate()
+{
+
+	Mat src, dst;
+	
+	char fname[MAX_PATH];
+	while (openFileDlg(fname))
+	{
+		src = imread(fname, IMREAD_GRAYSCALE);
+
+		Mat intermediate1 = Gaussian2D(src, 2);
+		Mat intermediate2 = sablon_filtru_ideal_high_pass(intermediate1);
+		Mat intermediate3 = auto_threshold(intermediate2);
+		Mat intermediate4 = deschidere(intermediate3, 1);
+
+		dst = maximumFilter(intermediate4);
+
+		// Get the current time again and compute the time difference [s]
+		//t = ((double)getTickCount() - t) / getTickFrequency();
+		// Print (in the console window) the processing time in [ms] 
+		//printf("Time = %.3f [ms]\n", t * 1000);
+
+		imshow("src", src);
+		imshow("dst", dst);
+
+		waitKey();
+	}
+}
+
 int main()
 {
 	int op;
+	char fname[MAX_PATH];
 	do
 	{
 		system("cls");
@@ -436,6 +871,10 @@ int main()
 		printf(" 7 - Edges in a video sequence\n");
 		printf(" 8 - Snap frame from live video\n");
 		printf(" 9 - Mouse callback demo\n");
+		printf(" 10 - Gaussian Blur\n");
+		printf(" 11 - Binary\n");
+		printf(" 12 - Filtru-Gauss High Pass\n");
+		printf(" 100 - Get License Plate\n");
 		printf(" 0 - Exit\n\n");
 		printf("Option: ");
 		scanf("%d",&op);
@@ -468,6 +907,36 @@ int main()
 				break;
 			case 9:
 				testMouseClick();
+				break;
+			case 10:
+				
+				while (openFileDlg(fname))
+				{
+					Mat src = imread(fname, IMREAD_GRAYSCALE);
+					imshow("dst", Gaussian2D(src, 2));
+
+					waitKey(0);
+				}
+				break;
+			case 11:
+				while (openFileDlg(fname))
+				{
+					Mat src = imread(fname, IMREAD_GRAYSCALE);
+					imshow("dst", auto_threshold(src));
+
+					waitKey(0);
+				}
+				break;
+			case 12:
+				while (openFileDlg(fname))
+				{
+					Mat src = imread(fname, IMREAD_GRAYSCALE);
+					imshow("dst", sablon_filtru_gaussian_high_pass(src));
+
+					waitKey(0);
+				}
+			case 100: 
+				getLicensePlate();
 				break;
 		}
 	}
